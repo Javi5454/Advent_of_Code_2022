e00c33b2ab7b75b78bb6d0aff4f71041bec95550
Javi5454 <javier-gomezlopez@outlook.es>
2022-12-09 01:28:22 +0100

    Day 6 Completed

---
+++ Day 6/problem2.js
@@ -0,0 +1,54 @@
+const { readSync } = require("fs");
+const { default: test } = require("node:test");
+
+fs = require("fs");
+
+//Returns position of the start-of-packet
+function start_of_message(text) {
+    var array = new Array(14);
+
+    var result = 14; //Base result
+
+    for (let i = 0; i < array.length; i++) { //First 4 letters
+        array[i] = text[i]
+    }
+
+    while (result < text.length) {
+        found = false; //Assume this case
+        for (let i = 0; i < array.length; i++) { //Looking for duplicates
+            for (let j = 0; j < array.length; j++) {
+                if (i != j && array[i] == array[j]) {
+                    found = true;
+                    break
+                }
+            }
+
+            if (found) { //Found duplicate
+                break;
+            }
+        }
+
+        if (!found) {
+            return result;
+        }
+        else {
+            array.shift(); //Remove first element and let scanning
+            array.push(text[result]);
+            result++;
+        }
+    }
+
+
+}
+
+fs.readFile('input1.txt', 'utf-8', function (err, data) {
+    if (err) {
+        console.log(err); //Error management
+    }
+
+    var text = data.toString();
+
+    var result = start_of_message(text);
+
+    console.log(result);
+})
